f8948a88f99ca991812d16676aa10a959cbc4404
yaowuya <zrfyaowuya@126.com>
2017-03-13 16:43:37 +0800

    修改测试文件

--- src/test/java/scau/com/lprapm/service/impl/PurOrderServiceImplTest.java
+++ src/test/java/scau/com/lprapm/service/impl/PurOrderServiceImplTest.java
@@ -13,14 +13,6 @@ import static org.junit.Assert.*;
  * Created by 钟锐锋 on 2017/2/8.
  */
 public class PurOrderServiceImplTest extends BaseTest{
-    @Autowired
-    PurOrderServiceImpl purOrderService;
-    @Test
-    public void searchPO() throws Exception {
-        Map<String,Object> params=new LinkedHashMap<>();
-        params.put("goodsName","冰块");
-        List<Map<String,Object>> list=purOrderService.searchPO(params);
-        System.out.println(list);
-    }
+
 
 }
